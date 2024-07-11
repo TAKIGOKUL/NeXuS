581aa262c0cf09b8c7105ce167cca0160acefe86
vipin-v <vipindas233@gmail.com>
2024-07-12 00:17:14 +0530

    set Login as default

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const FeedSection(), // Use the feed section page as the home screen
+      home: const LoginPage(), // Use the feed section page as the home screen
     );
   }
 }
