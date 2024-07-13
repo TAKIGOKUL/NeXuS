58082d3a8a8151c284025570ebf98db8511313ec
vipin-v <vipindas233@gmail.com>
2024-07-13 12:12:59 +0530

    applied lightColorScheme

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -7,6 +7,7 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Center(
